1d6400c87dad1740773df972fece0febf08410e3
Beatriz-Barbosa287 <bgb.beatrizgabrielabarbosa@gmail.com>
2025-11-14 08:15:32 -0300

    14/11/25

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+
 import App from './aplicacao/App'
 import Provedores from './aplicacao/provedores'
 import './styles/tokens.css'
 import './styles/base.css'
 import React from 'react'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Provedores>
-        <App />
-      </Provedores>
-    </BrowserRouter>
-  </StrictMode>
-)
